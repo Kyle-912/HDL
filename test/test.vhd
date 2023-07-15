c44753e8a7f522d8e97f342a33c8b11b0a401776
Kyle-912 <kdt912@outlook.com>
2023-07-15 15:43:03 -0400

    Added names to allow for easier future parsing

--- test/test.vhd
+++ test/test.vhd
@@ -1,45 +1,46 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-
 entity test is
-    port (
-        clk : in std_logic;
-        rst : in std_logic;
-        input : in std_logic_vector(7 downto 0);
-        done : out std_logic
+    port
+    (
+        clk   : in  std_logic;
+        rst   : in  std_logic;
+        go    : in  std_logic;
+        input : in  std_logic_vector(7 downto 0);
+        done  : out std_logic
     );
 end test;
-
 architecture bhv of test is
     type state_type is (IDLE, CHECK, DONE);
-    signal state : state_type;
+    signal state   : state_type;
     signal input_r : std_logic_vector(7 downto 0);
 begin
-    TEST_PROC : process(clk, rst)
+    TEST_PROC : process (clk, rst)
     begin
         if rst = '1' then
             input_r <= (others => '0');
-    
+            state   <= IDLE;
+            done    <= '0';
         elsif rising_edge(clk) then
+            input_r <= input;
             case state is
-            
                 when IDLE =>
-                    state <= CHECK;
-
+                    if go = '1' then
+                        state <= CHECK;
+                    end if;
                 when CHECK =>
                     if unsigned(input) = to_unsigned(0, input'length) then
-                        input_r <= input;
                         state <= DONE;
                     end if;
-
                 when DONE =>
                     done <= '1';
-
+                    if go = '0' then
+                        done  <= '0';
+                        state <= IDLE;
+                    end if;
                 when others => null;
-            
             end case;
-    
         end if;
     end process;
 end architecture;
