ade0320f0b88a276db92a1b29c7a8b17e16e00ab
Kyle-912 <kdt912@outlook.com>
2023-08-21 22:09:08 +0000

    Aug 21, 2023, 6:09 PM

--- Testing_Area/test2.vhd
+++ Testing_Area/test2.vhd
@@ -5,7 +5,8 @@ use ieee.numeric_std.all;
 entity ent is
   port (
     clk : in std_logic;
-    rst : in std_logic
+    rst : in std_logic;
+    input : in std_logic_vector(7 downto 0)
   );
 end ent;
 
