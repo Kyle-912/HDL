d219f65b315221cdd663454be3064ad30f9bd948
Kyle-912 <kdt912@outlook.com>
2023-08-21 22:08:40 +0000

    Aug 21, 2023, 6:08 PM

--- Testing_Area/test2.vhd
+++ Testing_Area/test2.vhd
@@ -5,24 +5,12 @@ use ieee.numeric_std.all;
 entity ent is
   port (
     clk : in std_logic;
-    rst : in std_logic;
-    input : in std_logic_vector(7 downto 0)
+    rst : in std_logic
   );
 end ent;
 
 architecture rtl of ent is
-signal input_r : std_logic_vector(7 downto 0);
+
 begin
-TEST_PROC : process(clk)
-begin
-  if rising_edge(clk) then
-    if rst = '1' then
-      
 
-    else
-
-
-    end if;
-  end if;
-end process;
 end architecture;
