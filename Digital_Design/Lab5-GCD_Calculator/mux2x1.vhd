eb21b8a77449aa319ed6fabe6addee041b389717
Kyle-912 <kdt912@outlook.com>
2023-08-21 19:59:51 +0000

    Aug 21, 2023, 3:59 PM

--- Digital_Design/Lab5-GCD_Calculator/mux2x1.vhd
+++ Digital_Design/Lab5-GCD_Calculator/mux2x1.vhd
@@ -4,19 +4,22 @@ use ieee.numeric_std.all;
 entity mux2x1 is
   generic (WIDTH : positive);
   port (
-    input0, input1 : in  std_logic_vector(WIDTH - 1 downto 0);
-    sel      : in  std_logic;
-    output   : out std_logic_vector(WIDTH - 1 downto 0)
+    input0 : in  std_logic_vector(WIDTH - 1 downto 0);
+    input1 : in  std_logic_vector(WIDTH - 1 downto 0);
+    sel    : in  std_logic;
+    output : out std_logic_vector(WIDTH - 1 downto 0)
   );
 end mux2x1;
-architecture mux2x1_BHV of mux2x1 is
+architecture BHV of mux2x1 is
 begin
   process (input0, input1, sel)
   begin
-    if (sel = '0') then
-      output <= input0;
-    else
-      output <= input1;
-    end if;
+    case (sel) is
+      when '0' =>
+        output <= input0;
+      when '1' =>
+        output <= input1;
+      when others => null;
+    end case;
   end process;
 end architecture;
