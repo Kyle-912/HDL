a2ebe8a3eff0f530813025673743ae9af1b7413f
Kyle-912 <kdt912@outlook.com>
2023-09-10 16:50:59 +0000

    Sep 10, 2023, 12:50 PM

--- Reconfigurable_Computing/Lab0/vhdl_tutorial_files/datapath.vhd
+++ Reconfigurable_Computing/Lab0/vhdl_tutorial_files/datapath.vhd
@@ -3,18 +3,18 @@ use ieee.std_logic_1164.all;
 
 entity datapath is
   generic (
-    width     :     positive := 16);
+    width : positive := 16);
   port (
     clk       : in  std_logic;
     rst       : in  std_logic;
     en        : in  std_logic;
     valid_in  : in  std_logic;
     valid_out : out std_logic;
-    in1       : in  std_logic_vector(width-1 downto 0);
-    in2       : in  std_logic_vector(width-1 downto 0);
-    in3       : in  std_logic_vector(width-1 downto 0);
-    in4       : in  std_logic_vector(width-1 downto 0);
-    output    : out std_logic_vector(width*2 downto 0));
+    in1       : in  std_logic_vector(width - 1 downto 0);
+    in2       : in  std_logic_vector(width - 1 downto 0);
+    in3       : in  std_logic_vector(width - 1 downto 0);
+    in4       : in  std_logic_vector(width - 1 downto 0);
+    output    : out std_logic_vector(width * 2 downto 0));
 end datapath;
 
 -- TODO: Implement the structural description of the datapath shown in
