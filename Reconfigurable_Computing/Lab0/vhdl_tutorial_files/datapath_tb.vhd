b620ea118c143844d74ef4b801e204944cd9e456
Kyle-912 <kdt912@outlook.com>
2023-09-10 18:06:28 +0000

    Sep 10, 2023, 2:06 PM

--- Reconfigurable_Computing/Lab0/vhdl_tutorial_files/datapath_tb.vhd
+++ Reconfigurable_Computing/Lab0/vhdl_tutorial_files/datapath_tb.vhd
@@ -11,28 +11,28 @@ end datapath_tb;
 
 architecture TB of datapath_tb is
 
-  constant TEST_WIDTH  : positive := 8;
-  constant MAX_INT_VAL : positive := 2**TEST_WIDTH-1;
+  constant TEST_WIDTH  : positive  := 8;
+  constant MAX_INT_VAL : positive  := 2 ** TEST_WIDTH - 1;
 
-  signal clk       : std_logic := '0';
-  signal rst       : std_logic;
-  signal en        : std_logic;
-  signal valid_in  : std_logic;
-  signal valid_out : std_logic;
-  signal in1       : std_logic_vector(TEST_WIDTH-1 downto 0);
-  signal in2       : std_logic_vector(TEST_WIDTH-1 downto 0);
-  signal in3       : std_logic_vector(TEST_WIDTH-1 downto 0);
-  signal in4       : std_logic_vector(TEST_WIDTH-1 downto 0);
-  signal output    : std_logic_vector(TEST_WIDTH*2 downto 0);
+  signal clk           : std_logic := '0';
+  signal rst           : std_logic;
+  signal en            : std_logic;
+  signal valid_in      : std_logic;
+  signal valid_out     : std_logic;
+  signal in1           : std_logic_vector(TEST_WIDTH - 1 downto 0);
+  signal in2           : std_logic_vector(TEST_WIDTH - 1 downto 0);
+  signal in3           : std_logic_vector(TEST_WIDTH - 1 downto 0);
+  signal in4           : std_logic_vector(TEST_WIDTH - 1 downto 0);
+  signal output        : std_logic_vector(TEST_WIDTH * 2 downto 0);
 
-  signal done : integer := 0;
+  signal done          : integer := 0;
 
-begin  -- TB
+begin -- TB
 
   UUT : entity work.datapath
-    generic map (
-      width     => TEST_WIDTH)
-    port map (
+    generic map(
+      width => TEST_WIDTH)
+    port map(
       clk       => clk,
       rst       => rst,
       en        => en,
@@ -50,51 +50,78 @@ begin  -- TB
   process
 
     procedure randInt(variable seed1, seed2 : inout positive;
-                       min, max              : in    integer;
-                       result                : out   integer) is
-      
-      variable                  rand         :       real;
-    begin
-      uniform(seed1, seed2, rand);
-      result := integer(real(min)+(rand*(real(max)-real(min))));
-    end procedure;
+    min, max                                : in    integer;
+    result                                  : out   integer) is
 
-    function checkOutput (
-      in1, in2, in3, in4 : std_logic_vector(TEST_WIDTH-1 downto 0))
-      return unsigned is
-
-      variable temp1, temp2 : unsigned(TEST_WIDTH*2-1 downto 0);
-    begin
-      temp1 := unsigned(in1)*unsigned(in2);
-      temp2 := unsigned(in3)*unsigned(in4);
-      return resize(temp1, TEST_WIDTH*2+1)+resize(temp2, TEST_WIDTH*2+1);
-    end checkOutput;
-
-    variable seed1, seed2 : positive;
-    variable temp_int     : integer;
-
+    variable rand                           : real;
   begin
-    rst      <= '1';
-    en       <= '1';
-    in1      <= (others => '0');
-    in2      <= (others => '0');
-    in3      <= (others => '0');
-    in4      <= (others => '0');
-    valid_in <= '0';
+    uniform(seed1, seed2, rand);
+    result := integer(real(min) + (rand * (real(max) - real(min))));
+  end procedure;
 
-    for i in 0 to 4 loop
-      wait until clk'event and clk = '1';
-    end loop;  -- i
+  function checkOutput(
+    in1, in2, in3, in4 : std_logic_vector(TEST_WIDTH - 1 downto 0))
+    return unsigned is
 
-    rst <= '0';
+    variable temp1, temp2 : unsigned(TEST_WIDTH * 2 - 1 downto 0);
+  begin
+    temp1 := unsigned(in1) * unsigned(in2);
+    temp2 := unsigned(in3) * unsigned(in4);
+    return resize(temp1, TEST_WIDTH * 2 + 1) + resize(temp2, TEST_WIDTH * 2 + 1);
+  end checkOutput;
+
+  variable seed1, seed2 : positive;
+  variable temp_int     : integer;
+
+begin
+  rst      <= '1';
+  en       <= '1';
+  in1      <= (others => '0');
+  in2      <= (others => '0');
+  in3      <= (others => '0');
+  in4      <= (others => '0');
+  valid_in <= '0';
+
+  for i in 0 to 4 loop
     wait until clk'event and clk = '1';
+  end loop; -- i
 
-    -- test overflows
+  rst <= '0';
+  wait until clk'event and clk = '1';
 
-    in1 <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
-    in2 <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
-    in3 <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
-    in4 <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
+  -- test overflows
+
+  in1      <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
+  in2      <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
+  in3      <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
+  in4      <= std_logic_vector(to_unsigned(MAX_INT_VAL, TEST_WIDTH));
+
+  valid_in <= '1';
+  wait until clk'event and clk = '1';
+  valid_in <= '0';
+  wait until clk'event and clk = '1';
+  wait until clk'event and clk = '1';
+
+  assert(valid_out = '1') report "Valid_out incorrect" severity warning;
+  assert(unsigned(output) = checkOutput(in1, in2, in3, in4))
+  report "Output incorrect : " &
+    integer'image(to_integer(unsigned(output))) & " ! = " &
+    integer'image(to_integer(checkOutput(in1, in2, in3, in4)))
+    severity warning;
+  wait until clk'event and clk = '1';
+  wait until clk'event and clk = '1';
+
+  -- test a bunch of random numbers
+
+  for i in 0 to 100 loop
+    randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
+    in1 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
+    randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
+    in2 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
+    randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
+    in3 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
+    randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
+    in4      <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
 
     valid_in <= '1';
     wait until clk'event and clk = '1';
@@ -104,46 +131,19 @@ begin  -- TB
 
     assert(valid_out = '1') report "Valid_out incorrect" severity warning;
     assert(unsigned(output) = checkOutput(in1, in2, in3, in4))
-      report "Output incorrect : " &
+    report "Output incorrect : " &
       integer'image(to_integer(unsigned(output))) & " ! = " &
       integer'image(to_integer(checkOutput(in1, in2, in3, in4)))
       severity warning;
     wait until clk'event and clk = '1';
     wait until clk'event and clk = '1';
 
-    -- test a bunch of random numbers
+  end loop; -- i
 
-    for i in 0 to 100 loop
-      randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
-      in1 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
-      randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
-      in2 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
-      randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
-      in3 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
-      randInt(seed1, seed2, 0, MAX_INT_VAL, temp_int);
-      in4 <= std_logic_vector(to_unsigned(temp_int, TEST_WIDTH));
+  done <= 1;
+  report "SIMULATION FINISHED!!!";
+  wait;
 
-      valid_in <= '1';
-      wait until clk'event and clk = '1';
-      valid_in <= '0';
-      wait until clk'event and clk = '1';
-      wait until clk'event and clk = '1';
-
-      assert(valid_out = '1') report "Valid_out incorrect" severity warning;
-      assert(unsigned(output) = checkOutput(in1, in2, in3, in4))
-        report "Output incorrect : " &
-        integer'image(to_integer(unsigned(output))) & " ! = " &
-        integer'image(to_integer(checkOutput(in1, in2, in3, in4)))
-        severity warning;
-      wait until clk'event and clk = '1';
-      wait until clk'event and clk = '1';
-
-    end loop;  -- i
-
-    done <= 1;
-    report "SIMULATION FINISHED!!!";
-    wait;
-
-  end process;
+end process;
 
 end TB;
