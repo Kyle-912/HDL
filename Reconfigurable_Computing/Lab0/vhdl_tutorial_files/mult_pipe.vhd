16a8a23b9c3ecf8aafd806f49ff9e35b890dfe62
Kyle-912 <kdt912@outlook.com>
2023-09-10 18:54:19 +0000

    Sep 10, 2023, 2:54 PM

--- Reconfigurable_Computing/Lab0/vhdl_tutorial_files/mult_pipe.vhd
+++ Reconfigurable_Computing/Lab0/vhdl_tutorial_files/mult_pipe.vhd
@@ -30,7 +30,7 @@ begin
 
     elsif rising_edge(clk) then
       if en = '1' then
-        output_r <= std_logic_vector(unsigned(in1) * unsigned(in2));
+        output_r <= std_logic_vector(unsigned(in1) * unsigned(in2)+1);
       end if;
 
     end if;
