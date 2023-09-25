9ae20b0be1d4b75804a9d615925f7fb529569c7c
Kyle-912 <kdt912@outlook.com>
2023-09-25 23:45:03 +0000

    Sep 25, 2023, 7:45 PM

--- Reconfigurable_Computing/Lab1/fsm.vhd
+++ Reconfigurable_Computing/Lab1/fsm.vhd
@@ -30,7 +30,7 @@ architecture default_arch of fsm is
   signal done_r              : std_logic;
 begin
 
-  done <= done_r;
+  -- done <= done_r;
 
   process (clk, rst)
   begin
@@ -88,7 +88,7 @@ begin
         end if;
 
       when RESTART =>
-        done_r <= '1';
+        done <= '1';
         if go = '0' then
           next_state <= START;
         end if;
