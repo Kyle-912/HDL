945dc4339097faf759fef7e8d3a8ca23ea751ee0
Kyle-912 <kdt912@outlook.com>
2023-09-25 23:17:01 +0000

    Sep 25, 2023, 7:17 PM

--- Reconfigurable_Computing/Lab1/fsm.vhd
+++ Reconfigurable_Computing/Lab1/fsm.vhd
@@ -71,20 +71,8 @@ begin
       when COMPUTE =>
         if n_eq_0 = '1' then
           result_sel <= '0';
-          result_en  <= '1';
-        else
-          while i_le_n = '1' loop
-            i_sel <= '1';
-            i_en  <= '1';
-            x_sel <= '1';
-            x_en  <= '1';
-            y_sel <= '1';
-            y_en  <= '1';
-          end loop;
-            if i_le_n = '1' then
-              
-            else
-              
+          result_en  <= '1';            if i_le_n = '1' then
+
             end if;
           next_state <= RESTART;
         end if;
