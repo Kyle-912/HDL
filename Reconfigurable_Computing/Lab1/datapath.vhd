36208aa9544822fd010a9d198a492a93c490ecfd
Kyle-912 <kdt912@outlook.com>
2023-09-25 20:28:36 +0000

    Sep 25, 2023, 4:28 PM

--- Reconfigurable_Computing/Lab1/datapath.vhd
+++ Reconfigurable_Computing/Lab1/datapath.vhd
@@ -83,3 +83,18 @@ begin
     end if;
   end process;
 end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity subtractor is
+  generic (WIDTH : positive);
+  port (
+    in1, in2 : in  std_logic_vector(WIDTH - 1 downto 0);
+    output   : out std_logic_vector(WIDTH - 1 downto 0)
+  );
+end subtractor;
+architecture subtractor_BHV of subtractor is
+begin
+  output <= std_logic_vector(unsigned(in1) - unsigned(in2));
+end architecture;
