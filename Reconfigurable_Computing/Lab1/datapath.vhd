94b36681a1003b68d4c229b8696ec3aa52d2bdab
Kyle-912 <kdt912@outlook.com>
2023-09-25 20:25:52 +0000

    Sep 25, 2023, 4:25 PM

--- Reconfigurable_Computing/Lab1/datapath.vhd
+++ Reconfigurable_Computing/Lab1/datapath.vhd
@@ -29,3 +29,29 @@ begin
 
 end default_arch;
 
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mux2x1 is
+  generic (WIDTH : positive);
+  port (
+    input0 : in  std_logic_vector(WIDTH - 1 downto 0);
+    input1 : in  std_logic_vector(WIDTH - 1 downto 0);
+    sel    : in  std_logic;
+    output : out std_logic_vector(WIDTH - 1 downto 0)
+  );
+end mux2x1;
+architecture BHV of mux2x1 is
+begin
+  process (input0, input1, sel)
+  begin
+    case (sel) is
+      when '0' =>
+        output <= input0;
+      when '1' =>
+        output <= input1;
+      when others => null;
+    end case;
+  end process;
+end architecture;
