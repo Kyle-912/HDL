27c72c4eeddb303511264f9b16743df822cb6bc3
Kyle-912 <kdt912@outlook.com>
2023-09-25 20:26:38 +0000

    Sep 25, 2023, 4:26 PM

--- Reconfigurable_Computing/Lab1/datapath.vhd
+++ Reconfigurable_Computing/Lab1/datapath.vhd
@@ -59,7 +59,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 entity reg is
-  generic (WIDTH : positive);
+  generic (WIDTH : positive := 6);
   port (
     clk    : in  std_logic;
     rst    : in  std_logic;
