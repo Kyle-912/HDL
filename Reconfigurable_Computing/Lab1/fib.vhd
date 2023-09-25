52ebd5aa01dda24608e08fb4827f792c7b29fcf5
Kyle-912 <kdt912@outlook.com>
2023-09-25 19:47:52 +0000

    Sep 25, 2023, 3:47 PM

--- Reconfigurable_Computing/Lab1/fib.vhd
+++ Reconfigurable_Computing/Lab1/fib.vhd
@@ -42,8 +42,8 @@ begin
           if go = '1' then
             done_r  <= '0';
             n_r     <= n;
-            x_r     <= std_logic_vector(to_unsigned(0, x_r'length));
-            y_r     <= std_logic_vector(to_unsigned(1, y_r'length));
+            x_r <= std_logic_vector(to_unsigned(0, x_r'length));
+            y_r <= std_logic_vector(to_unsigned(1, y_r'length));
             state_r <= COMPUTE;
           end if;
 
