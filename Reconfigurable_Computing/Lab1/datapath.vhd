4b47efd6f8e5c9d7f7f9914a239fdd170d91e02c
Kyle-912 <kdt912@outlook.com>
2023-09-25 20:30:16 +0000

    Sep 25, 2023, 4:30 PM

--- Reconfigurable_Computing/Lab1/datapath.vhd
+++ Reconfigurable_Computing/Lab1/datapath.vhd
@@ -98,3 +98,30 @@ architecture adder_BHV of adder is
 begin
   output <= std_logic_vector(unsigned(in1) + unsigned(in2));
 end architecture;
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity comparator is
+  generic (WIDTH : positive);
+  port (
+    x, y           : in  std_logic_vector(WIDTH - 1 downto 0);
+    x_lt_y, x_ne_y : out std_logic
+  );
+end comparator;
+architecture comparator_BHV of comparator is
+begin
+  process (x, y)
+  begin
+    if (unsigned(x) < unsigned(y)) then
+      x_lt_y <= '1';
+    else
+      x_lt_y <= '0';
+    end if;
+    if (unsigned(x) /= unsigned(y)) then
+      x_ne_y <= '1';
+    else
+      x_ne_y <= '0';
+    end if;
+  end process;
+end architecture;
