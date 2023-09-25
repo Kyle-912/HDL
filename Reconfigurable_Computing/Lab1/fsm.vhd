bc9a3c93f6aff2c54c6124dcfaa1a45a8a9510d1
Kyle-912 <kdt912@outlook.com>
2023-09-25 17:19:47 +0000

    Sep 25, 2023, 1:19 PM

--- Reconfigurable_Computing/Lab1/fsm.vhd
+++ Reconfigurable_Computing/Lab1/fsm.vhd
@@ -2,28 +2,28 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity fsm is
-    port (
-        clk  : in  std_logic;
-        rst  : in  std_logic;
-        go   : in  std_logic;
-        done : out std_logic;
+  port (
+    clk        : in  std_logic;
+    rst        : in  std_logic;
+    go         : in  std_logic;
+    done       : out std_logic;
 
-        n_en       : out std_logic;
-        result_en  : out std_logic;
-        result_sel : out std_logic;
-        x_en       : out std_logic;
-        x_sel      : out std_logic;
-        y_en       : out std_logic;
-        y_sel      : out std_logic;
-        i_en       : out std_logic;
-        i_sel      : out std_logic;
+    n_en       : out std_logic;
+    result_en  : out std_logic;
+    result_sel : out std_logic;
+    x_en       : out std_logic;
+    x_sel      : out std_logic;
+    y_en       : out std_logic;
+    y_sel      : out std_logic;
+    i_en       : out std_logic;
+    i_sel      : out std_logic;
 
-        n_eq_0 : in std_logic;
-        i_le_n : in std_logic
-        );
+    n_eq_0     : in  std_logic;
+    i_le_n     : in  std_logic
+  );
 end fsm;
 
-architecture default_arch of fsm is    
+architecture default_arch of fsm is
 begin
 
 end default_arch;
