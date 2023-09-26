c01fa06e7f7a488862de1e222071eb5289139e10
Kyle-912 <kdt912@outlook.com>
2023-09-26 00:40:40 +0000

    Sep 25, 2023, 8:40 PM

--- Reconfigurable_Computing/Lab1/fib_tb.vhd
+++ Reconfigurable_Computing/Lab1/fib_tb.vhd
@@ -3,10 +3,51 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.math_real.all;
 
+use std.textio.all;
+use std.env.finish;
+
 entity fib_tb is
 end fib_tb;
 
 architecture tb of fib_tb is
+
+  constant clk_hz     : integer   := 100e6;
+  constant clk_period : time      := 1 sec / clk_hz;
+
+  signal clk          : std_logic := '1';
+  signal rst          : std_logic := '1';
+  signal go : std_logic := '0';
+  signal done : std_logic;
+  signal n : std_logic_vector(5 downto 0);
+  signal result : std_logic_vector(31 downto 0);
+
 begin
+
+  clk <= not clk after clk_period / 2;
+
+  DUT : entity work.fib
+    port map(
+      clk    => clk,
+      rst    => rst,
+      go     => go,
+      n      => n,
+      result => result,
+      done   => done
+
+    );
+
+  SEQUENCER_PROC : process
+  begin
+    wait for clk_period * 2;
+
+    rst <= '0';
+
+    wait for clk_period * 10;
+    assert false
+    report "Replace this with your test cases"
+      severity failure;
+
+    finish;
+  end process;
 
 end tb;
