31d91d28526a9169e1768471f8cb50306e1bd3c4
Kyle-912 <kdt912@outlook.com>
2023-10-09 20:48:35 +0000

    Oct 9, 2023, 4:48 PM

--- Reconfigurable_Computing/Lab2/part2/hdl/peripheral_test.vhd
+++ Reconfigurable_Computing/Lab2/part2/hdl/peripheral_test.vhd
@@ -1,4 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity peripheral_test is
+  generic (width : positive);
+  port (
+    in0  : in  std_logic_vector(width - 1 downto 0);
+    in1  : in  std_logic_vector(width - 1 downto 0);
+    in2  : in  std_logic_vector(width - 1 downto 0);
+    in3  : in  std_logic_vector(width - 1 downto 0);
+    out0 : out std_logic_vector(width - 1 downto 0);
+    out1 : out std_logic_vector(width - 1 downto 0);
+    out2 : out std_logic_vector(width - 1 downto 0);
+    out3 : out std_logic_vector(width - 1 downto 0)
+  );
+end peripheral_test;
+
+architecture default_arch of peripheral_test is
+begin
+
+end default_arch;
 
 
 
