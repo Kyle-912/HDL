a23efdf3aed939a7c09ecbc512a3ed5692cd6b17
Kyle-912 <kdt912@outlook.com>
2023-10-09 21:12:34 +0000

    Oct 9, 2023, 5:12 PM

--- Reconfigurable_Computing/Lab2/part2/hdl/peripheral_test_tb.vhd
+++ Reconfigurable_Computing/Lab2/part2/hdl/peripheral_test_tb.vhd
@@ -54,9 +54,13 @@ begin
     assert out0 = std_logic_vector(to_unsigned(6, out0'length))
     report "Incorrect out0"
       severity failure;
-    assert out1 = std_logic_vector(to_unsigned(5, out0'length))
+    assert out1 = std_logic_vector(to_unsigned(5, out1'length))
     report "Incorrect out0"
       severity failure;
+    assert out2 = std_logic_vector(to_unsigned(5, out2'length))
+    report "Incorrect out0"
+      severity failure;
+      
 
     finish;
   end process;
