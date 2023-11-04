87f37415ccf04748cb8b900aca493c5928152b92
Kyle-912 <kdt912@outlook.com>
2023-11-04 19:44:11 +0000

    Nov 4, 2023, 3:44 PM

--- Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
+++ Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
@@ -8,7 +8,6 @@ use work.user_pkg.all;
 entity datapath is
   port (
     clk       : in  std_logic;
-    rst       : in  std_logic;
     valid_in  : in  std_logic;
     data_in   : in  std_logic_vector(C_MEM_IN_WIDTH - 1 downto 0);
     valid_out : out std_logic;
@@ -18,19 +17,10 @@ end datapath;
 
 architecture bhv of datapath is
 begin
-    CALC_PROC : process(clk, rst)
-    begin
-        if rst = '1' then
-            -- in0 <= (others => '0');
-            -- in1 <= (others => '0');
-            -- in2 <= (others => '0');
-            -- in3 <= (others => '0');
-
-        elsif rising_edge(clk) then
-            data_out <= std_logic_vector((unsigned(data_in(31 downto 24)) * unsigned(data_in(23 downto 16))) + (unsigned(data_in(15 downto 8)) * unsigned(data_in(7 downto 0))));
-
-        end if;
-    end process;
+  process (clk)
+  begin
+    data_out <= std_logic_vector(resize((unsigned(data_in(31 downto 24)) * unsigned(data_in(23 downto 16))), data_out'length) + (unsigned(data_in(15 downto 8)) * unsigned(data_in(7 downto 0))));
+  end process;
 end architecture;
 
 -- data_in(7 downto 0)
