f244131fefadb3ff47d2916ec6e7aa551fdb3457
Kyle-912 <kdt912@outlook.com>
2023-11-04 19:46:26 +0000

    Nov 4, 2023, 3:46 PM

--- Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
+++ Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
@@ -40,3 +40,4 @@ end architecture;
 -- data_in(15 downto 8)
 -- data_in(23 downto 16)
 -- data_in(31 downto 24)
+-- data_out <= std_logic_vector(resize((unsigned(data_in(31 downto 24)) * unsigned(data_in(23 downto 16))), data_out'length) + (unsigned(data_in(15 downto 8)) * unsigned(data_in(7 downto 0))));
