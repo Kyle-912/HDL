be1651ff92b26e8747292d94dc4682e7b08958ce
Kyle-912 <kdt912@outlook.com>
2023-11-04 20:34:00 +0000

    Nov 4, 2023, 4:34 PM

--- Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
+++ Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
@@ -17,13 +17,13 @@ entity datapath is
 end datapath;
 
 architecture bhv of datapath is
-  signal in0_r      : std_logic_vector(7 downto 0);
-  signal in1_r      : std_logic_vector(7 downto 0);
-  signal in2_r      : std_logic_vector(7 downto 0);
-  signal in3_r      : std_logic_vector(7 downto 0);
-  signal product0_r : std_logic_vector(15 downto 0);
-  signal product1_r : std_logic_vector(15 downto 0);
-  signal sum_r      : std_logic_vector(16 downto 0);
+  signal in0_r        : std_logic_vector(7 downto 0);
+  signal in1_r        : std_logic_vector(7 downto 0);
+  signal in2_r        : std_logic_vector(7 downto 0);
+  signal in3_r        : std_logic_vector(7 downto 0);
+  signal product0_r   : std_logic_vector(15 downto 0);
+  signal product1_r   : std_logic_vector(15 downto 0);
+  signal sum_r        : std_logic_vector(16 downto 0);
   signal valid_delay1 : std_logic;
   signal valid_delay2 : std_logic;
   signal valid_delay3 : std_logic;
@@ -31,27 +31,28 @@ begin
   CALC_PROC : process (clk, rst)
   begin
     if rst = '1' then
-      in0_r      <= (others => '0');
-      in1_r      <= (others => '0');
-      in2_r      <= (others => '0');
-      in3_r      <= (others => '0');
-      product0_r <= (others => '0');
-      product1_r <= (others => '0');
-      sum_r      <= (others => '0');
+      in0_r        <= (others => '0');
+      in1_r        <= (others => '0');
+      in2_r        <= (others => '0');
+      in3_r        <= (others => '0');
+      product0_r   <= (others => '0');
+      product1_r   <= (others => '0');
+      sum_r        <= (others => '0');
       valid_delay1 <= '0';
-      valid_delay1 <= '0';
-      valid_delay1 <= '0';
+      valid_delay2 <= '0';
+      valid_delay3 <= '0';
 
     elsif rising_edge(clk) then
       in0_r      <= data_in(31 downto 24);
       in1_r      <= data_in(23 downto 16);
       in2_r      <= data_in(15 downto 8);
       in3_r      <= data_in(7 downto 0);
+      
 
       product0_r <= std_logic_vector(unsigned(in0_r) * unsigned(in1_r));
       product1_r <= std_logic_vector(unsigned(in2_r) * unsigned(in3_r));
 
-      sum_r <= std_logic_vector(resize(unsigned(product0_r), sum_r'length) + unsigned(product1_r));
+      sum_r      <= std_logic_vector(resize(unsigned(product0_r), sum_r'length) + unsigned(product1_r));
     end if;
   end process;
   data_out <= sum_r;
