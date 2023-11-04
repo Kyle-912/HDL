d348aeddc378607a5f7ccb861c95e4e5f7bc666c
Kyle-912 <kdt912@outlook.com>
2023-11-04 22:29:37 +0000

    Nov 4, 2023, 6:29 PM

--- Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
+++ Reconfigurable_Computing/Lab4/pipeline_1.0/src/datapath.vhd
@@ -27,7 +27,6 @@ architecture bhv of datapath is
   signal valid_delay1 : std_logic;
   signal valid_delay2 : std_logic;
   signal valid_delay3 : std_logic;
-  signal valid_delay4 : std_logic;
 begin
   process (clk, rst)
   begin
@@ -56,10 +55,8 @@ begin
 
       sum_r        <= std_logic_vector(resize(unsigned(product0_r), sum_r'length) + unsigned(product1_r));
       valid_delay3 <= valid_delay2;
-
-      valid_delay4 <= valid_delay3;
     end if;
   end process;
   data_out  <= sum_r;
-  valid_out <= valid_delay4;
+  valid_out <= valid_delay3;
 end architecture;
